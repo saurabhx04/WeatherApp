79b8312a82c9fb22c52ac25ecb3ef2168fec766c
Saurabh Singh <saurabhx04@gmail.com>
2024-08-22 01:37:52 +0530

    update .gitignore file

--- weather.py
+++ weather.py
@@ -1,4 +1,6 @@
 import requests
+import datetime 
+
 #api_key = "56e544883e62521a536c81c0a7967a0d"
 api_key = "aff4546cfa968d59b4e160a0b1fa5acd"
 
@@ -22,14 +24,23 @@ if data["cod"] == 200:
     main = data["main"]
     weather = data["weather"][0]
     wind = data["wind"]
-    #sunrise = data["sunrise"]
-    #sunset = data["sunset"]
+    sunrise_sunset = data["sys"]
+    #sunset = data["sys"]
+     
+    #sunrise = datetime.datetime.fromtimestamp(sunrise_sunset['sunrise'])
+    #sunset = datetime.datetime.fromtimestamp(sunrise_sunset['sunset'])
+    
 
     # Print the weather information
     print(f"City: {city_name}")
     print(f"Temperature: {main['temp']}°C")
+    print(f"Feels Like: {main['feels_like']}°C")
     print(f"Weather: {weather['description']}")
     print(f"Humidity: {main['humidity']}%")
     print(f"Wind Speed: {wind['speed']} m/s")
+    #print(f"Sunset : {sunset}")
+    #print(f"Sunrise : {sunrise}")
+    print(f"Sunset : {datetime.datetime.fromtimestamp(sunrise_sunset['sunrise'])}")
+    print(f"Sunrise : {datetime.datetime.fromtimestamp(sunrise_sunset['sunset'])}")
 else:
     print("City not found, please check the name and try again.")
